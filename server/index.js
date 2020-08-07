a2860eec949dfcdc0daf6fcee4ec141ec1f52e1d
Michael Brecht <brecht.michael.b@gmail.com>
2020-08-07 09:45:56 -0600

    (init)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static());
+app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
